f149b450f32d09da3111a3fe1e229cd110897e2d
Chris Stewart <stewart.chris1234@gmail.com>
2019-05-25 08:41:48 -0500

    Attempt to sync with sendheaders

--- node-test/src/test/scala/org/bitcoins/node/SpvNodeTest.scala
+++ node-test/src/test/scala/org/bitcoins/node/SpvNodeTest.scala
@@ -2,15 +2,14 @@ package org.bitcoins.node
 
 import akka.actor.ActorSystem
 import org.bitcoins.core.crypto.DoubleSha256DigestBE
+import org.bitcoins.rpc.client.common.BitcoindRpcClient
 import org.bitcoins.rpc.util.RpcUtil
 import org.bitcoins.testkit.node.NodeUnitTest
-import org.bitcoins.testkit.node.fixture.{
-  NodeFixture,
-  SpvNodeConnectedWithBitcoind
-}
+import org.bitcoins.testkit.node.fixture.SpvNodeConnectedWithBitcoind
 import org.scalatest.FutureOutcome
 
 import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
 
 class SpvNodeTest extends NodeUnitTest {
 
@@ -37,7 +36,7 @@ class SpvNodeTest extends NodeUnitTest {
         bitcoind.generate(1).map(_.head)
       }
 
-      //check we have that hash inside of our chain project!
+      //sync our spv node expecting to get that generated hash
       val spvSyncF = for {
         _ <- hashF
         sync <- spvNode.sync()
@@ -55,6 +54,50 @@ class SpvNodeTest extends NodeUnitTest {
           .retryUntilSatisfiedF(isSameBestHash)
           .map(_ => succeed)
       }
+  }
 
+  it must "stay in sync with a bitcoind instance" in {
+    spvNodeConnectedWithBitcoind: SpvNodeConnectedWithBitcoind =>
+      val spvNode = spvNodeConnectedWithBitcoind.spvNode
+      val bitcoind = spvNodeConnectedWithBitcoind.bitcoind
+
+      //this needs to be called to get our peer to send us headers
+      //as they happen with the 'sendheaders' message
+      //both our spv node and our bitcoind node _should_ both be at the genesis block (regtest)
+      //at this point so no actual syncing is happening
+      val initSyncF = spvNode.sync()
+
+      //start generating a block every 10 seconds with bitcoind
+      //this should result in 5 blocks
+      val startGenF = initSyncF.map { _ =>
+        Thread.sleep(5000)
+        //generate a block every 5 seconds
+        //until we have generated 5 total blocks
+        genBlock5seconds(bitcoind)
+      }
+
+      startGenF.flatMap { _ =>
+        Thread.sleep(40000)
+
+        //we should expect 5 headers have been announced to us via
+        //the send headers message.
+        spvNode.chainApi.getBlockCount.map(count => assert(count == 5))
+      }
+  }
+
+  private var counter = 0
+  private def genBlock5seconds(bitcoind: BitcoindRpcClient)(
+      implicit system: ActorSystem): Unit = {
+    val genBlock = new Runnable {
+      override def run(): Unit = {
+        if (counter < 5) {
+          bitcoind.generate(1)
+          counter = counter + 1
+        } else {
+          //do nothing
+        }
+      }
+    }
+    system.scheduler.schedule(5.seconds, 5.seconds, genBlock)
   }
 }
