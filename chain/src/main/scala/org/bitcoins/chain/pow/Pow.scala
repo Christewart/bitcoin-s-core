f6289a94226bd63eeb95af64b4fccedf3280f147
Chris Stewart <stewart.chris1234@gmail.com>
2019-07-21 14:09:02 -0500

    implement second special case for if we are allowing min difficulty blocks, but are at a POW change interval. If we are at a POW change interval, we return that block

--- chain/src/main/scala/org/bitcoins/chain/pow/Pow.scala
+++ chain/src/main/scala/org/bitcoins/chain/pow/Pow.scala
@@ -45,6 +45,7 @@ sealed abstract class Pow extends BitcoinSLogger {
           val nonMinDiffF = blockHeaderDAO.find { h =>
             h.nBits != powLimit || h.height % chainParams.difficultyChangeInterval == 0
           }
+
           nonMinDiffF.map {
             case Some(bh) => bh.nBits
             case None     =>
