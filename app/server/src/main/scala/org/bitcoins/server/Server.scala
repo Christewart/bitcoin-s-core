b82c9a37b84f915a5d08fd8a7dc9eecc97e18982
christewart <stewart.chris1234@gmail.com>
2020-06-05 12:32:29 -0500

    WIP

--- app/server/src/main/scala/org/bitcoins/server/Server.scala
+++ app/server/src/main/scala/org/bitcoins/server/Server.scala
@@ -7,7 +7,10 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.directives.DebuggingDirectives
+import akka.http.scaladsl.settings.ServerSettings
+import akka.util.ByteString
 import de.heikoseeberger.akkahttpupickle.UpickleSupport._
+import org.bitcoins.core.util.BitcoinSLogger
 import org.bitcoins.db.AppConfig
 import upickle.{default => up}
 
@@ -17,13 +20,13 @@ case class Server(
     conf: AppConfig,
     handlers: Seq[ServerRoute],
     rpcport: Int = 9999)(implicit system: ActorSystem)
-    extends HttpLogger {
-  implicit private val config: AppConfig = conf
+    extends BitcoinSLogger {
+  implicit private lazy val config: AppConfig = conf
 
   import system.dispatcher
 
   /** Handles all server commands by throwing a MethodNotFound */
-  private val catchAllHandler: PartialFunction[ServerCommand, StandardRoute] = {
+  private lazy val catchAllHandler: PartialFunction[ServerCommand, StandardRoute] = {
     case ServerCommand(name, _) => throw HttpError.MethodNotFound(name)
   }
 
@@ -59,9 +62,9 @@ case class Server(
     }
   }
 
-  val route =
+  def route: Route = {
     // TODO implement better logging
-    DebuggingDirectives.logRequestResult("http-rpc-server", Logging.InfoLevel) {
+/*    DebuggingDirectives.logRequestResult("http-rpc-server", Logging.InfoLevel) {
       withErrorHandling {
         pathSingleSlash {
           post {
@@ -75,15 +78,40 @@ case class Server(
           }
         }
       }
-    }
+    }*/
+    ???
+  }
 
-  def start(): Future[Http.ServerBinding] = {
-    val httpFut =
-      Http().bindAndHandle(route, "localhost", rpcport)
+  def start(): Future[Http.ServerBinding] = try {
+    println(s"Begin")
+    val entity = HttpEntity.Empty
+    val response = HttpResponse(status = 503,
+      entity = entity)
+    require(response != null, s"Response was null")
+    println(s"Http response not null")
+    require(response.entity != null, s"response.entity was null")
+    println(s"response.entity not null")
+    require(response.entity.isKnownEmpty(), s"response.entity.isKnownEmpty() was null")
+    println(s"not null isKnownEmpty()")
+    val serverSettings = ServerSettings(system)
+      .withVerboseErrorMessages(true)
+      .withTerminationDeadlineExceededResponse(response)
+    println(s"Server settings=${serverSettings.toString}")
+    val httpFut = {
+      Http().bindAndHandle(handler = route,
+        interface = "localhost",
+        port = rpcport,
+        settings = serverSettings)
+    }
+    println(s"Starting Bitcoin-S HTTP server")
     httpFut.foreach { http =>
       logger.info(s"Started Bitcoin-S HTTP server at ${http.localAddress}")
     }
     httpFut
+  } catch {
+    case scala.util.control.NonFatal(exn) =>
+      println(s"EXN THROWN ${exn}")
+      sys.exit(1)
   }
 }
 
