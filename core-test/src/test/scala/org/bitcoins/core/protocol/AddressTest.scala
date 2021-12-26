0a2a41fa13ebb43512a4f8327432e9bc3d07bae6
Chris Stewart <stewart.chris1234@gmail.com>
2021-12-26 11:42:09 -0600

    Add decompressed pubkey -> p2pkh address example

--- core-test/src/test/scala/org/bitcoins/core/protocol/AddressTest.scala
+++ core-test/src/test/scala/org/bitcoins/core/protocol/AddressTest.scala
@@ -1,5 +1,7 @@
 package org.bitcoins.core.protocol
 
+import org.bitcoins.core.config.MainNet
+import org.bitcoins.crypto.ECPublicKey
 import org.bitcoins.testkitcore.gen.AddressGenerator
 import org.bitcoins.testkitcore.util.{BitcoinSUnitTest, TestUtil}
 
@@ -35,5 +37,14 @@ class AddressTest extends BitcoinSUnitTest {
     forAll(AddressGenerator.address) { addr =>
       assert(addr.descriptor == s"addr(${addr.value})")
     }
+  }
+
+  it must "generate a p2pkh address from a decompressed public key" in {
+    //insert uncompressed pubkey here
+    val hex =
+      "044f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa385b6b1b8ead809ca67454d9683fcf2ba03456d6fe2c4abe2b07f0fbdbb2f1c1"
+    val pubKey = ECPublicKey.fromHex(hex)
+    val p2pkh = P2PKHAddress(pubKey, MainNet)
+    println(s"address=${p2pkh.toString}")
   }
 }
