7de773fb8fc62e43b25e66144847b535e2bc3dc6
Chris Stewart <stewart.chris1234@gmail.com>
2022-07-04 16:39:03 -0500

    Cleanup logging/println

--- core/src/main/scala/org/bitcoins/core/protocol/script/ScriptWitness.scala
+++ core/src/main/scala/org/bitcoins/core/protocol/script/ScriptWitness.scala
@@ -399,7 +399,6 @@ object TaprootScriptPath {
     val merkleRoot = computeTaprootMerkleRoot(controlBlock, tapLeafHash)
 
     val parity = (controlBlock.bytes.head & 1) == 1
-    println(s"parity=$parity")
     program.pubKey.checkTapTweak(internal = internalPubKey,
                                  merkleRootOpt = Some(merkleRoot),
                                  parity = parity)
