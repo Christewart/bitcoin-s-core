0c120d078b2460e9ba586e2ca0f420ce2fdb615f
Chris Stewart <stewart.chris1234@gmail.com>
2024-01-20 15:16:40 -0600

    Fix endHeight

--- app/scripts/src/main/scala/org/bitcoins/scripts/ScanBitcoind.scala
+++ app/scripts/src/main/scala/org/bitcoins/scripts/ScanBitcoind.scala
@@ -175,7 +175,7 @@ class ScanBitcoind()(implicit
   }
 
   def countAllScriptNums(bitcoind: BitcoindRpcClient): Future[Unit] = {
-    val blockCountF = Future.successful(130000) //bitcoind.getBlockCount()
+    val blockCountF = bitcoind.getBlockCount()
     val sourceF = blockCountF.map(h => Source((1.until(h))))
     val fn: Block => Vector[ScriptNumHelper] = { block =>
       block.transactions.map(findScriptNum).flatten.toVector
