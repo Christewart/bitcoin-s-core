1751d2d8e2f4fe3a2a11dcd8abd6ecac7a89d632
Chris Stewart <stewart.chris1234@gmail.com>
2024-01-22 18:03:57 -0600

    micropickle.read() directly from Path

--- app/scripts/src/main/scala/org/bitcoins/scripts/ScriptNumHelper.scala
+++ app/scripts/src/main/scala/org/bitcoins/scripts/ScriptNumHelper.scala
@@ -4,7 +4,7 @@ import org.bitcoins.core.protocol.transaction.Transaction
 import org.bitcoins.core.script.constant.ScriptConstant
 import org.bitcoins.crypto.DoubleSha256DigestBE
 
-import java.nio.file.Paths
+import java.nio.file.{Paths}
 
 case class ScriptNumHelper(
     txIdBE: DoubleSha256DigestBE,
