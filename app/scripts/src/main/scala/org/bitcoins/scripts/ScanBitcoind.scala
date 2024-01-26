7eef740ca2c17b2d36873268561eeca64c54e188
Chris Stewart <stewart.chris1234@gmail.com>
2024-01-26 15:20:10 -0600

    Add correct startHeight=2404716

--- app/scripts/src/main/scala/org/bitcoins/scripts/ScanBitcoind.scala
+++ app/scripts/src/main/scala/org/bitcoins/scripts/ScanBitcoind.scala
@@ -160,7 +160,7 @@ class ScanBitcoind()(implicit
 
   def countAllScriptNums(bitcoind: BitcoindRpcClient): Future[Unit] = {
     val blockCountF = bitcoind.getBlockCount()
-    val sourceF = blockCountF.map(h => Source((1.until(h))))
+    val sourceF = blockCountF.map(h => Source((2404716.until(h))))
     val fn: Block => Vector[ScriptNumHelper] = { block =>
       block.transactions.map(findScriptNum).flatten.toVector
     }
