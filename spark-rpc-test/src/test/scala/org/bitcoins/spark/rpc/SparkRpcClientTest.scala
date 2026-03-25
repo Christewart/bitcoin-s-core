53134e1df85d8bf4ffcaff5434e97294627d01c7
Chris Stewart <stewart.chris1234@gmail.com>
2026-03-24 19:19:45 -0500

    Get spark grpc authentication working

--- spark-rpc-test/src/test/scala/org/bitcoins/spark/rpc/SparkRpcClientTest.scala
+++ spark-rpc-test/src/test/scala/org/bitcoins/spark/rpc/SparkRpcClientTest.scala
@@ -1,7 +1,7 @@
 package org.bitcoins.spark.rpc
 
 import com.google.protobuf.ByteString
-import org.bitcoins.crypto.ECPublicKey
+import org.bitcoins.crypto.{ECPrivateKey, ECPublicKey}
 import org.bitcoins.spark.rpc.proto.spark.GenerateDepositAddressRequest
 import org.bitcoins.testkit.util.BitcoinSAsyncTest
 import scodec.bits.ByteVector
@@ -15,19 +15,29 @@ class SparkRpcClientTest extends BitcoinSAsyncTest {
 
   implicit def byteStringToByteVec(byteString: ByteString): ByteVector =
     ByteVector(byteString.toByteArray)
+
   it must "generate a deposit address" in {
-    val instance = SparkInstanceLocal(new java.net.URI("http://localhost:8080"))
+    val instance =
+      SparkInstanceLocal(
+        new java.net.URI("https://localhost:8535"),
+        trustSelfSigned = true
+      )
     val client = SparkRpcClient(instance)
+
+    // Identity key is used for challenge-response auth with the operator.
+    // Signing key is the secp256k1 key associated with the deposit address.
+    val identityKey = ECPrivateKey.freshPrivateKey
     val signingKey = ECPublicKey.freshPublicKey
-    val identityKey = ECPublicKey.freshPublicKey
+
     val req = GenerateDepositAddressRequest(
       signingPublicKey = signingKey.bytes,
-      identityPublicKey = identityKey.bytes
+      identityPublicKey = identityKey.publicKey.bytes
     )
     for {
+      _ <- client.login(identityKey)
       info <- client.generateDepositAddress(req)
     } yield {
-      println(s"Generated address: ${info}")
+      println(s"Generated address: $info")
       succeed
     }
   }
